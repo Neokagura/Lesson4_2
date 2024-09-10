4b6ce555176418271a6140df50192f0c121e582c
Ness <bur>
2024-09-10 19:17:34 +0300

    Добавьте файлы проекта.

---
+++ Lesson4_2/Lesson4_2.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int day;
+    cout << "Введите число: ";
+    cin >> day;
+    if (day == 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 20, 21, 27, 28) {
+        cout << "Это выходной день\n";
+
+    }
+    else {
+        cout << "Это рабочий день";
+    }
+
+      
+}
+
